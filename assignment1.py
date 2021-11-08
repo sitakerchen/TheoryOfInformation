fbf147a5e724ca1220df98d8d23bbc1b48cb6fcf
sitakerchen <sitaker@yahoo.com>
2021-11-08 22:58:26 +0800

    delete some privacy info

--- assignment1.py
+++
@@ -1,7 +0,0 @@
-import FLib as f
-
-if __name__ == '__main__':
-    f.add_pro_auto('x1y1,1/8;x1y2,1/16;x2y1,3/16;x2y2,10/16;x1,3/16;x2,13/16;y1,5/16;y2,11/16')
-    f.add_pro_auto('y1,1/8')
-    f.show_allMyPro()
-    f.print_b()
